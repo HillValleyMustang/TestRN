4865d9585756fb8935a3c4f101bf531446a243dc
[dyad] <craig.duffill@gmail.com>
2025-08-27 17:12:31 +0100

    [dyad] wrote 3 file(s)

--- src/app/(app)/workout/page.tsx
+++ src/app/(app)/workout/page.tsx
@@ -458,7 +458,7 @@ export default function WorkoutPage() {
 
   // Render the WorkoutSelector component with the props
   return (
-    <div className="p-4 sm:p-8">
+    <div className="p-4 sm:p-8"> {/* Corrected className prop placement */}
       <h1 className="text-3xl font-bold mb-6">Start Your Workout</h1> {/* Added page heading */}
       <WorkoutSelector 
         {...workoutFlowManagerProps} 
