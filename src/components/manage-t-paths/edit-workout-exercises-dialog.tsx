4d096613d153338ac61db2448d77039e8cca5464
[dyad] <craig.duffill@gmail.com>
2025-08-31 15:24:04 +0100

    [dyad] Widened internal components of the EditWorkoutExercisesDialog modal - wrote 1 file(s)

--- src/components/manage-t-paths/edit-workout-exercises-dialog.tsx
+++ src/components/manage-t-paths/edit-workout-exercises-dialog.tsx
@@ -300,18 +300,18 @@ export const EditWorkoutExercisesDialog = ({
 
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
-      <DialogContent className="sm:max-w-4xl max-h-[90vh] flex flex-col"> {/* Increased max-w to 4xl */}
-        <DialogHeader className="p-4 pb-2"> {/* Reduced horizontal padding */}
+      <DialogContent className="sm:max-w-[900px] max-h-[90vh] flex flex-col">
+        <DialogHeader className="p-4 pb-2">
           <DialogTitle className="text-2xl font-bold flex items-center gap-2">
             Manage Exercises for <WorkoutBadge workoutName={workoutName} className="text-xl px-3 py-1" />
           </DialogTitle>
         </DialogHeader>
 
-        <div className="flex-grow overflow-y-auto px-4 pb-4 space-y-4"> {/* Reduced horizontal padding */}
+        <div className="flex-grow overflow-y-auto px-4 pb-4 space-y-4">
           {loading ? (
             <p className="text-muted-foreground">Loading exercises...</p>
           ) : (
-            <div className="space-y-6">
+            <div className="space-y-6 w-full"> {/* Added w-full here */}
               <div className="flex gap-2 w-full">
                 <Select onValueChange={setSelectedExerciseToAdd} value={selectedExerciseToAdd}>
                   <SelectTrigger className="w-full"><SelectValue placeholder="Add exercise" /></SelectTrigger>
