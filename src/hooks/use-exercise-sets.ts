4d5776b418c5056123669e9e2d5610642a3b3dbe
[dyad] <craig.duffill@gmail.com>
2025-08-31 21:04:27 +0100

    [dyad] Fixed console error by refining RPC error logging in `useExerciseSets` hook. - wrote 1 file(s)

--- src/hooks/use-exercise-sets.ts
+++ src/hooks/use-exercise-sets.ts
@@ -141,7 +141,7 @@ export const useExerciseSets = ({
           p_user_id: session.user.id,
           p_exercise_id: exerciseId,
         });
-        if (rpcError) {
+        if (rpcError && rpcError.message) { // Only log if there's an actual error message
           console.error(`Error fetching last sets for exercise ${exerciseName}:`, rpcError);
         } else if (lastExerciseSets) {
           lastSetsMap.set(exerciseId, lastExerciseSets);
